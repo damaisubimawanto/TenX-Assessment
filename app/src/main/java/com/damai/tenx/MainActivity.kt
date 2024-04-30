65ba9fe897aa94c58e987529d5e6d072e53c153e
damaisubimawanto <damai.subimawanto@gmail.com>
2024-04-30 15:30:06 +0700

    Initial project. - Add PixelFiveByFive class.

---
+++ app/src/main/java/com/damai/tenx/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.damai.tenx
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.damai.tenx.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val pixelFiveByFive = PixelFiveByFive()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnCase1.setOnClickListener {
+            val result = pixelFiveByFive.removeNonColorfulPixel(
+                givenPixelList = listOf(
+                    Triple(34, 203, 55),
+                    Triple(67, 76, 73),
+                    Triple(99, 105, 93),
+                    Triple(178, 173, 169),
+                    Triple(144, 89, 54),
+
+                    Triple(22, 20, 18),
+                    Triple(10, 40, 50),
+                    Triple(171, 180, 211),
+                    Triple(150, 150, 90),
+                    Triple(50, 150, 150),
+
+                    Triple(209, 109, 107),
+                    Triple(111, 117, 212),
+                    Triple(214, 113, 165),
+                    Triple(45, 137, 212),
+                    Triple(182, 240, 245),
+
+                    Triple(199, 184, 72),
+                    Triple(204, 75, 193),
+                    Triple(140, 132, 139),
+                    Triple(87, 76, 63),
+                    Triple(170, 209, 167),
+
+                    Triple(1, 90, 20),
+                    Triple(174, 214, 174),
+                    Triple(196, 106, 112),
+                    Triple(173, 166, 167),
+                    Triple(48, 35, 46),
+                )
+            )
+
+            val resultText = "Result = $result"
+            binding.tvResultCase1.text = resultText
+        }
+
+        binding.btnCase2.setOnClickListener {
+            val result = pixelFiveByFive.determineColorfulImage(
+                givenPixelList = listOf(
+                    Triple(87, 76, 63),
+                    Triple(67, 76, 73),
+                    Triple(99, 105, 93),
+                    Triple(178, 173, 169),
+                    Triple(48, 35, 46),
+
+                    Triple(22, 20, 18),
+                    Triple(10, 40, 50),
+                    Triple(67, 76, 73),
+                    Triple(173, 166, 167),
+                    Triple(87, 76, 63),
+
+                    Triple(10, 40, 50),
+                    Triple(99, 105, 93),
+                    Triple(178, 173, 169),
+                    Triple(67, 76, 73),
+                    Triple(22, 20, 18),
+
+                    Triple(22, 20, 18),
+                    Triple(87, 76, 63),
+                    Triple(140, 132, 139),
+                    Triple(87, 76, 63),
+                    Triple(99, 105, 93),
+
+                    Triple(99, 105, 93),
+                    Triple(87, 76, 63),
+                    Triple(67, 76, 73),
+                    Triple(173, 166, 167),
+                    Triple(48, 35, 46),
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/damai/tenx/PixelFiveByFive.kt
@@ -0,0 +1,153 @@
+package com.damai.tenx
+
+import android.util.Log
+
+/**
+ * Created by damai007 on 30/April/2024
+ */
+class PixelFiveByFive {
+
+    private fun Int.convertToRange(): Double {
+        return this / HIGH_PIXEL
+    }
+
+    private fun average(pixel: Triple<Int, Int, Int>): Int {
+        return (pixel.first + pixel.second + pixel.third) / 3
+    }
+
+    private fun max(pixel: Triple<Int, Int, Int>): Double {
+        var result = 0.0
+        pixel.first.convertToRange().let { red ->
+            if (red > result) {
+                result = red
+            }
+        }
+        pixel.second.convertToRange().let { green ->
+            if (green > result) {
+                result = green
+            }
+        }
+        pixel.third.convertToRange().let { blue ->
+            if (blue > result) {
+                result = blue
+            }
+        }
+        return result
+    }
+
+    private fun min(pixel: Triple<Int, Int, Int>): Double {
+        var result = 1.0
+        pixel.first.convertToRange().let { red ->
+            if (red < result) {
+                result = red
+            }
+        }
+        pixel.second.convertToRange().let { green ->
+            if (green < result) {
+                result = green
+            }
+        }
+        pixel.third.convertToRange().let { blue ->
+            if (blue < result) {
+                result = blue
+            }
+        }
+        return result
+    }
+
+    private fun getLuminosity(pixel: Triple<Int, Int, Int>): Double {
+        val bigM = max(pixel = pixel)
+        val smallM = min(pixel = pixel)
+        return (bigM + smallM) / 2
+    }
+
+    private fun getHue(pixel: Triple<Int, Int, Int>): Int {
+        val red = pixel.first.convertToRange()
+        val green = pixel.second.convertToRange()
+        val blue = pixel.third.convertToRange()
+        val result = when {
+            //(A) If R >= G >= B
+            green in blue..red -> {
+                (60 * ( (green - blue) / (red - blue) )).toInt()
+            }
+
+            //(B) If G > R >= B
+            green > red && red >= blue -> {
+                (60 * ( 2 - ( (red - blue) / (green - blue) ) )).toInt()
+            }
+
+            //(C) If G >= B > R
+            green >= blue && blue > red -> {
+                (60 * ( 2 + ( (blue - red) / (green - red) ) )).toInt()
+            }
+
+            //(D) If B > G > R
+            blue > green && green > red -> {
+                (60 * ( 4 - ( (green - red) / (blue - red) ) )).toInt()
+            }
+
+            //(E) If B > R >= G
+            blue > red && red > green -> {
+                (60 * ( 4 + ( (red - green) / (blue - green) ) )).toInt()
+            }
+
+            //(F) If R >= B > G
+            red >= blue && blue > green -> {
+                (60 * ( 6 - ( (blue - green) / (red - green) ) )).toInt()
+            }
+
+            else -> 0
+        }
+
+        return result
+    }
+
+    private fun getSaturation(
+        luminosity: Double,
+        pixel: Triple<Int, Int, Int>
+    ): Double {
+        return when {
+            luminosity < 1.0 -> {
+                (max(pixel = pixel) - min(pixel = pixel)) / (1 - ((2 * luminosity) - 1))
+            }
+
+            luminosity == 1.0 -> 0.0
+
+            else -> -1.0
+        }
+    }
+
+    fun removeNonColorfulPixel(
+        givenPixelList: List<Triple<Int, Int, Int>>
+    ): List<Triple<Int, Int, Int>> {
+        val resultList = mutableListOf<Triple<Int, Int, Int>>()
+        for (pixel in givenPixelList) {
+            val average = average(pixel = pixel)
+            val luminosity = getLuminosity(pixel = pixel)
+            val hue = getHue(pixel = pixel)
+            val saturation = getSaturation(
+                luminosity = luminosity,
+                pixel = pixel
+            ).let {
+                (it * 100).toInt()
+            }
+            Log.d("zxczxc", "pixel = $pixel, luminosity = ${(luminosity * 100).toInt()}%, hue = $hue degree, saturation = ${(saturation * 100).toInt()}%")
+
+            if (saturation > 20) {
+                resultList.add(pixel)
+            }
+        }
+        Log.d("zxczxc", "\n\nResult list = $resultList")
+        return resultList.toList()
+    }
+
+    fun determineColorfulImage(
+        givenPixelList: List<Triple<Int, Int, Int>>
+    ): Boolean {
+        return true
+    }
+
+    companion object {
+        private const val HIGH_PIXEL = 255.0
+    }
+}
